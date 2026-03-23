46c7011d864528782101af06b501493b04824ed2
rafiqwe <rmlmanik502@gmail.com>
2026-03-24 01:12:01 +0600

    Created: Splash page

--- lib/UI/splash/splash_page.dart
+++ lib/UI/splash/splash_page.dart
@@ -1,9 +1,27 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:spotify/domain/app_color.dart';
+import 'package:spotify/domain/app_route.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(
+      Duration(seconds: 3),
+      () => Navigator.pushReplacementNamed(context, AppRoute.intro_page),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
