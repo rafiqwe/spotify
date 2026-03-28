586adecb90b3235cc5276f835d9181447c08d3bb
rafiqwe <rmlmanik502@gmail.com>
2026-03-28 21:34:00 +0600

    Add: library page

---
+++ lib/UI/dashboard/navigations/library_bottom_nav_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LibraryBottomNavPage extends StatelessWidget {
+  const LibraryBottomNavPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('Library Page')));
+  }
+}
