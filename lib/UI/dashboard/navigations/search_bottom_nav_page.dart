ecec2d074d8ddb5bfdaf35149f8419ded5668a7f
unknown <rmlmanik502@gmail.com>
2026-06-25 21:34:45 +0600

    Create: search bottom nav page

--- lib/UI/dashboard/navigations/search_bottom_nav_page.dart
+++ lib/UI/dashboard/navigations/search_bottom_nav_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:spotify/UI/custom_widgets/m_height.dart';
 import 'package:spotify/UI/custom_widgets/search_song_items.dart';
 import 'package:spotify/domain/app_color.dart';
@@ -9,31 +10,45 @@ class SearchBottomNavPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColor.blackColor,
-      appBar: AppBar(
+    return SafeArea(
+      child: Scaffold(
         backgroundColor: AppColor.blackColor,
-        centerTitle: true,
-        leading: Text(
-          'Search',
-          style: TextStyle(
-            fontSize: 19,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'bold',
-            color: AppColor.whiteColor,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Search',
+                      style: TextStyle(
+                        fontFamily: 'bold',
+                        fontSize: 22,
+                        color: AppColor.whiteColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SvgPicture.asset(
+                      'assets/icons/notification.svg',
+                      color: AppColor.whiteColor,
+                      height: 20,
+                      width: 20,
+                    ),
+                  ],
+                ),
+              ),
+              MHeight(mHeight: 15),
+              Padding(padding: const EdgeInsets.symmetric(horizontal: 10),
+              child:UiHelper.CustomTextFieldSearch(),),
+              MHeight(mHeight: 20),
+              SearchSongItems(Title: 'Your Top Genres', itemTexts: ['Pop', 'Indie'],),
+              SearchSongItems(Title: "Popular podcast categories", itemTexts: ['News & Politics','Comedy'],),
+              SearchSongItems(Title: 'Browse all', itemTexts: ['2026 Wrapped', 'Podcasts', 'Hello'],),
+            ],
           ),
         ),
-        actions: [Icon(Icons.camera_alt_outlined, size: 28)],
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10.0),
-            child: UiHelper.CustomTextFieldSearch(),
-          ),
-          MHeight(mHeight: 20),
-          SearchSongItems(),
-        ],
       ),
     );
   }
