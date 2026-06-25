d77ef1ab0159cf9e15ae9091950533c2b02ecf96
unknown <rmlmanik502@gmail.com>
2026-06-25 21:32:28 +0600

    Create playlist list component

--- lib/UI/custom_widgets/playlist_list_compo.dart
+++ lib/UI/custom_widgets/playlist_list_compo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:spotify/UI/custom_widgets/m_height.dart';
 import 'package:spotify/domain/app_color.dart';
 
 class PlaylistListCompo extends StatelessWidget {
@@ -17,32 +18,27 @@ class PlaylistListCompo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        SizedBox(
-          height: 70,
-          width: double.infinity,
-          child: ListTile(
-            leading:  Image.asset(
-              imagePath,
-              // fit: BoxFit.contain,
+        ListTile(
+          leading: Image.asset(
+            imagePath,
+          ),
+          title: Text(
+            title,
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w600,
+              color: AppColor.whiteColor
             ),
-            title: Text(
-              title,
-              style: TextStyle(
-                fontSize: 15,
-                fontWeight: FontWeight.w600,
-                color: AppColor.whiteColor
-              ),
-            ),
-            subtitle: Text(
-              subTitle,
-              style: TextStyle(
-                fontSize: 13,
-                color: AppColor.greyColor,
+          ),
+          subtitle: Text(
+            subTitle,
+            style: TextStyle(
+              fontSize: 13,
+              color: AppColor.greyColor,
 
-              ),
             ),
-          )
-        )
+          ),
+        ),
       ],
     );
   }
